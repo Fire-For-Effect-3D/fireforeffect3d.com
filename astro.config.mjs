831ef7db0e0a51df261fe194f364331be7e8d7ef
jgnewman97 <justin@jgnewman.com>
2025-10-16 20:39:22 -0500

    Point site assets at custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://fireforeffect3d.com',
+  base: '/',
   vite: {
     plugins: [tailwindcss()]
   }
